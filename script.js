863525e02980eb1c078ef1ae32321abb3646d88d
Joseph Essel Moses <jmosesessel21@gmail.com>
2023-05-19 00:21:26 +0000

    implement minute submission and optimize timer button clicks

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Get the button click event
-
-const countdownBtn = (e) => {
-	const timeInSec = e.dataset.time;
-	console.log(e.dataset.time);
-	timer(timeInSec);
-};
+// Get the timer__button button click event
+document.querySelectorAll('.timer__button').forEach(element=>{
+    element.addEventListener('click', (event)=>{
+        const timeInSec = event.target.getAttribute('data-time');
+        timer(timeInSec);       
+    })
+})
 
 let display__time_left = document.querySelector(".display__time-left");
 let display__end_time = document.querySelector(".display__end-time");
@@ -17,7 +17,8 @@ const timer = (seconds) => {
 	// Future time in milliseconds
 	const future = now + seconds * 1000;
 
-    displayCountDown(seconds);
+    // display countdown and return time
+	displayCountDown(seconds);
 	displayReturnTime(future);
 	countDown = setInterval(() => {
 		let secondsLeft = Math.round((future - Date.now()) / 1000);
@@ -30,6 +31,7 @@ const timer = (seconds) => {
 	}, 1000);
 };
 
+// display the count down timer
 function displayCountDown(secondsLeft) {
 	let minute = Math.floor(secondsLeft / 60);
 	let seconds = secondsLeft % 60;
@@ -39,10 +41,32 @@ function displayCountDown(secondsLeft) {
 
 // display the return time in --- futureTime in milliseconds
 function displayReturnTime(futureTime) {
-    let newDate = new Date(futureTime)
+	let newDate = new Date(futureTime);
 	let hours = newDate.getHours() % 12;
 	let minute = newDate.getMinutes();
 	let seconds = newDate.getSeconds();
-	let display = `Be Back At ${hours}:${minute < 10 ? "0" : ""}${minute}`;
+	let display = `Be Back At ${hours}:${minute < 10 ? "0" : ""}${minute}:${seconds < 10 ? "0" : ""}${seconds}`;
 	display__end_time.textContent = display;
 }
+
+// submit the form in minutes
+document
+	.querySelector(".submit__button")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+        const enteredMins = document.getElementsByName('minutes')[0].value
+        console.log('enteredMins',enteredMins)
+        // take only numbers
+        if(enteredMins == '' || isNaN(enteredMins)){
+            alert('Enter time in minutes')
+            enteredMins.value = ''
+        }
+        
+        // convert the minutes to seconds
+        const seconds = enteredMins * 60;
+        console.log('min in secs', seconds)
+
+        // call the timer function and pass the seconds
+        timer(seconds)
+        
+	});
